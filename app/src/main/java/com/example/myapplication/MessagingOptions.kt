8b0e9617874b932b1dd9b2ff352a87817eaad4ff
Leipo Yan <leipo.yan@solace.com>
2020-10-01 11:26:31 +0800

    Change broker configuraiton in MessagingOptions.kt

--- app/src/main/java/com/example/myapplication/MessagingOptions.kt
+++ app/src/main/java/com/example/myapplication/MessagingOptions.kt
@@ -6,9 +6,9 @@ package com.example.myapplication
 // Fill in your Solace Cloud PubSub+ Broker's 'MQTT Host' and 'Password' options.
 // This information can be found under:
 // https://console.solace.cloud/services/ -> <your-service> -> 'Connect' -> 'MQTT'
-const val SOLACE_CLIENT_USER_NAME = "solace-cloud-client"
-const val SOLACE_CLIENT_PASSWORD = "PLACEHOLDER_PASSWORD"
-const val SOLACE_MQTT_HOST = "tcp://PLACEHOLDER_SERVICE_ID.messaging.solace.cloud:1883"
+const val SOLACE_CLIENT_USER_NAME = "default"
+const val SOLACE_CLIENT_PASSWORD = "default"
+const val SOLACE_MQTT_HOST = "tcp://cloud.ylpsingapore.com:1883"
 
 // Other options
 const val SOLACE_CONNECTION_TIMEOUT = 3
